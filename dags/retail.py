b69273bd5cf6f481d637a5dbb935a50a97acc3be
Igor Langoni <123383171+igorlangoni@users.noreply.github.com>
2024-01-24 16:03:31 +0000

    Created tasks: create_bigquery_dataset, gcp_to_raw

--- dags/retail.py
+++ dags/retail.py
@@ -2,6 +2,11 @@ from airflow.decorators import dag, task
 from datetime import datetime
 
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
 
 
 @dag(
@@ -12,8 +17,37 @@ from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesyste
 )
 
 def retail():
+    # loads local file to gcs bucket
     upload_csv_to_gcs = LocalFilesystemToGCSOperator(
-        
+        task_id='upload_csv_to_gcs',
+        src='include/dataset/online_retail.csv',
+        dst='raw/online_retail.csv',
+        bucket='online_retail_project_igorl',
+        gcp_conn_id='gcp',
+        mime_type='text/csv',
+    )
+
+    # creates a bigquery dataset/schema
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
+
+    # loads file from gcs bucket into a table in the bigquery schema
+    gcs_to_raw = aql.load_file(
+        task_id='gcs_to_raw',
+        input_file=File(
+            'gs://online_retail_project_igorl/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=False,
     )
 
 retail()
