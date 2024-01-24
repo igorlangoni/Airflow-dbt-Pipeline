434f94c075d0b0ef642d56631524094205ed2fb0
Igor Langoni <123383171+igorlangoni@users.noreply.github.com>
2024-01-24 16:45:52 +0000

    Created check_load task

--- dags/retail.py
+++ dags/retail.py
@@ -50,4 +50,11 @@ def retail():
         use_native_support=False,
     )
 
+
+    @task.external_python(python='/Users/igorl/Projects/portfolio/airflow_dbt_pipeline/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
 retail()
