053d685bf06cc2d92e38d6856acc5816b3f3c31e
Igor Langoni <123383171+igorlangoni@users.noreply.github.com>
2024-01-24 15:23:58 +0000

    Deleted exemple DAGs and started working on Retail DAG

---
+++ dags/retail.py
@@ -0,0 +1,19 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=['retail'],
+)
+
+def retail():
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        
+    )
+
+retail()
