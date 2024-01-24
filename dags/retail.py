e57e4192e52265d86963965d1e24748639035d5a
Igor Langoni <123383171+igorlangoni@users.noreply.github.com>
2024-01-24 17:40:00 +0000

    Added the external python operator to run Soda checks

--- dags/retail.py
+++ dags/retail.py
@@ -51,10 +51,12 @@ def retail():
     )
 
 
-    @task.external_python(python='/Users/igorl/Projects/portfolio/airflow_dbt_pipeline/soda_venv/bin/python')
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
     def check_load(scan_name='check_load', checks_subpath='sources'):
         from include.soda.check_function import check
 
         return check(scan_name, checks_subpath)
+
+    check_load()
 
 retail()
